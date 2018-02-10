405461862311a6e92ad4d9ffaad350c5575959cf
xNoSkill <paulo2003@gmx.de>
2018-02-11 00:46:42 +0100

    s

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+// When the user clicks on div, open the popup
+function myFunctiontwo() {
+    var popup = document.getElementById("popupzwei");
+    popup.classList.toggle("show");
+}
+
+function PlaySound(soundobj) {
+                    var thissound = document.getElementById(soundobj);
+                    thissound.play();
+                }
+function StopSound(soundobj) {
+var thissound = document.getElementById(soundobj);
+thissound.pause();
+thissound.currentTime = 0;}
