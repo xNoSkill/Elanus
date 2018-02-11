7416fc70813d0a9488603d0941c9e35589eba823
xNoSkill <paulo2003@gmx.de>
2018-02-11 01:11:14 +0100

    s

--- javascript.js
+++ javascript.js
@@ -13,7 +13,8 @@ function PlaySound(soundobj) {
                     var thissound = document.getElementById(soundobj);
                     thissound.play();
                 }
+
 function StopSound(soundobj) {
 var thissound = document.getElementById(soundobj);
 thissound.pause();
-thissound.currentTime = 0;}
+   thissound.currentTime = 0;}
